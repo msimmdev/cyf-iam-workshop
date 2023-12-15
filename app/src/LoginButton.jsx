deac794213c932b230c1057e1fa76d96bb7ad0e2
Michael Simm <michael@simm.dev>
2023-12-15 19:39:55 +0000

    Implement auth into the react app.

--- app/src/LoginButton.jsx
+++ app/src/LoginButton.jsx
@@ -1,7 +1,26 @@
-import { Button } from "@chakra-ui/react";
+import { Button, Spinner } from "@chakra-ui/react";
+import { useAuth } from "oidc-react";
 
-export default () => (
-  <Button bg="green" margin={10}>
-    Log In
-  </Button>
-);
+export default () => {
+  const auth = useAuth();
+
+  console.log(auth.userData);
+
+  if (auth.isLoading) {
+    return <Spinner />;
+  }
+
+  if (auth && auth.userData) {
+    return (
+      <Button bg="red" margin={10} onClick={() => auth.signOut()}>
+        Log Out
+      </Button>
+    );
+  }
+
+  return (
+    <Button bg="green" margin={10} onClick={() => auth.signIn()}>
+      Log In
+    </Button>
+  );
+};
